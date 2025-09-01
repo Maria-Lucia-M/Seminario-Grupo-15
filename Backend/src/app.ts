afc5c860e2bba41cf8b472ba9c983b88d7cbc3de
Manuel <ferrarismanu@gmail.com>
2025-09-01 20:40:29 -0300

    Primer commit

---
+++ Backend/src/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import { Request, Response } from "express";
+const app = express();
+
+app.use('/', (req:Request, res:Response)=> {
+    res.send("Holaa!!!");
+});
+
+app.listen(3000, ()=> {
+    console.log('Server running on http://localhost:3000/');
+});
