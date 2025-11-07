b32d1141ac33665aaf23d9a9ff496f87b0ede15e
Pedro Nicolás <pnicolas@frro.utn.edu.ar>
2025-11-06 22:22:02 -0300

    CUU4-Fontend version inicial

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import CUU4Page from './pages/CUU4-Page';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/cuu/colocacion-vacunas" element={<CUU4Page />} />
       {/* otras rutas */}
     </Routes>
   );
---
+++ Frontend/src/pages/CUU4-Page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ColocVacunas from "../components/CUU4/ColocVacunas";
+
+const CUU4Page: React.FC = () => {
+    return <ColocVacunas />;
+};
+
+export default CUU4Page;
